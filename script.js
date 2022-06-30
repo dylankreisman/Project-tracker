cca94fc2db9adeb076c6dc933a7aafff5cfb5ac8
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-06-30 11:26:19 -0700

    display current time in Jumbotron

---
+++ script.js
@@ -0,0 +1,14 @@
+var datetime = null,
+        date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('#current-time')
+    update();
+    setInterval(update, 1000);
+});
+
